804484e0c6637d8ecfa52ffffd14747dbcea3df8
Danilo Santos <dansansi@hotmail.com>
2026-01-19 23:26:03 -0300

    Ajustes iniciais

--- Tecboard-React/src/App.jsx
+++ Tecboard-React/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
 import "./App.css";
 
+function FormularioDeEvento() {
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="">Qual o nome do evento?</label>
+        <input type="text" id="nome" />
+      </fieldset>
+    </form>
+  );
+}
 function App() {
   return (
     <main>
       <header>
-        <img src="" alt="" />
-        <section>
-          <img src="" alt="" />
-        </section>
+        <img src="/logo.png" alt="" />
       </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+      <FormularioDeEvento />
     </main>
   );
 }
