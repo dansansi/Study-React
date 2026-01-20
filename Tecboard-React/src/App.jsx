f47c49b48cfa154b8d5226e8160b601ce3b40ac7
Danilo Santos <dansansi@hotmail.com>
2026-01-19 22:18:12 -0300

    primeiro commit

---
+++ Tecboard-React/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="" alt="" />
+        <section>
+          <img src="" alt="" />
+        </section>
+      </header>
+    </main>
+  );
+}
+
+export default App;
